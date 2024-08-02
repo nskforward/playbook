29a64a105b67e560fe8a43362fd8d4a9506fb023
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-03 00:19:02 +0300

    init

--- cmd/file_write.go
+++ cmd/file_write.go
@@ -10,9 +10,9 @@ import (
 func FileWrite(c *conn.Conn, append bool, file, body string) {
 	var output string
 	if append {
-		output = c.Execute(fmt.Sprintf("sh -c 'echo \"%s\" >> %s' && echo \"true\"", body, file))
+		output = c.Execute(fmt.Sprintf("sh -c 'echo \"%s\" >> %s'", body, file))
 	} else {
-		output = c.Execute(fmt.Sprintf("sh -c 'echo \"%s\" > %s' && echo \"true\"", body, file))
+		output = c.Execute(fmt.Sprintf("sh -c 'echo \"%s\" > %s'", body, file))
 	}
 	if output != "" {
 		util.Check(fmt.Errorf("cmd.FileWrite failed: %s", output))

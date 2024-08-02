0fa571e980f42130b4ef4849ff25d292aebeda3b
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-02 23:54:53 +0300

    init

--- conn/connect.go
+++ conn/connect.go
@@ -64,7 +64,8 @@ func Connect(host, user string, args ...LoginArg) *Conn {
 	util.Confitm()
 
 	conn := dial(cfg)
-
+	fmt.Println("----------------------")
 	fmt.Println("successfully connected")
+	fmt.Println("----------------------")
 	return conn
 }

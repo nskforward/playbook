64c99351646f3cfc5236ccf3159ada45c2b262b1
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-03 01:14:48 +0300

    init

--- scenario/ssh_disable_root.go
+++ scenario/ssh_disable_root.go
@@ -1,10 +1,20 @@
 package scenario
 
 import (
+	"fmt"
+
 	"github.com/nskforward/playbook/cmd"
 	"github.com/nskforward/playbook/conn"
 )
 
 func SSHDisableRoot(c *conn.Conn) {
+	fmt.Println("# DISABLE ROOT SSH LOGIN")
+
+	fmt.Println("--> edit ssh config")
 	cmd.SSHConfig(c, "PermitRootLogin", "no")
+	fmt.Println("<-- ok")
+
+	fmt.Println("--> restart sshd service")
+	cmd.Systemctl(c, "sshd", cmd.Restart)
+	fmt.Println("<-- ok")
 }

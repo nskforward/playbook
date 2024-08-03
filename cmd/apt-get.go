663b3a9f5de5d7e0ab2878ef5ed30e00719a9836
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-03 08:22:23 +0300

    init

--- cmd/apt-get.go
+++ cmd/apt-get.go
@@ -16,11 +16,11 @@ var (
 )
 
 func AptGet(c *conn.Conn, action AptGetAction, args ...string) {
-	items := []string{"apt-get", "--quiet", "--yes", string(action)}
+	items := []string{"apt-get", "--quiet", "--yes", string(action), "&&", "echo", "success"}
 	items = append(items, args...)
 	command := strings.Join(items, " ")
 	output := c.Execute(command)
-	if output != "" {
+	if output != "" && !strings.HasSuffix(output, "success") {
 		util.Check(fmt.Errorf("cmd.AptGet failed: %s", output))
 	}
 }

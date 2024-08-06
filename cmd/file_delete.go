415b33358035cf13fe92eb4c4be9ac46c938240d
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-06 20:06:10 +0300

    rm multiple files

--- cmd/file_delete.go
+++ cmd/file_delete.go
@@ -2,10 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/nskforward/playbook/conn"
 )
 
-func FileDelete(c *conn.Conn, path string) {
-	c.Execute(fmt.Sprintf("rm %s", path))
+func FileDelete(c *conn.Conn, path ...string) {
+	c.Execute(fmt.Sprintf("rm %s", strings.Join(path, " ")))
 }

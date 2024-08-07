03eaf78cc478b5e642b2340775e1b35d53655883
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-07 22:43:33 +0300

    update scenario ssd disable root login

--- scenario/ssh_disable_root.go
+++ scenario/ssh_disable_root.go
@@ -10,8 +10,15 @@ import (
 func SSHDisableRoot(c *conn.Conn) {
 	fmt.Println("# DISABLE ROOT SSH LOGIN")
 
-	fmt.Println("--> edit ssh config")
-	cmd.SSHConfig(c, "PermitRootLogin", "no")
+	fmt.Println("--> check if setting already exists")
+	if cmd.FileExists(c, "/etc/ssh/ssh_config.d/disable_root_login.conf") {
+		fmt.Println("<-- exists")
+		return
+	}
+	fmt.Println("<-- does not exist")
+
+	fmt.Println("--> adding")
+	c.Execute(`echo "PermitRootLogin no" | sudo tee /etc/ssh/ssh_config.d/disable_root_login.conf`)
 	fmt.Println("<-- ok")
 
 	fmt.Println("--> restart sshd service")
